a1a05aedf8f1da37ff019fb28c279fa753da8d20
aaanwar24 <126901552+aaanwar24@users.noreply.github.com>
2023-04-03 11:57:27 -0700

    I'm trying to add more enemies but my code's broken

--- my_game/sprites.py
+++ my_game/sprites.py
@@ -1,17 +1,24 @@
+
+# File created by Abdullah Anwar
 import pygame as pg
 
 from pygame.sprite import Sprite
 
 from settings import *
 
+from random import randint
+
 vec = pg.math.Vector2
+
 
 # create a player
 
 class Player(Sprite):
-    def __init__(self):
+    def __init__(self, game):
         Sprite.__init__(self)
+        self.game = game
         self.image = pg.Surface((50,50))
+        # self.image = pg.transform.scale((50, 38))
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
         self.pos = vec(WIDTH/2, HEIGHT/2)
@@ -29,11 +36,21 @@ class Player(Sprite):
             self.acc.y = PLAYER_ACC
         if keystate[pg.K_d]:
             self.acc.x = PLAYER_ACC
+    # def jump(self):
+    #     # jump only if standing on a platform
+    #     self.rect.x += 1
+    #     hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+    #     self.rect.x -= 1
+    #     if hits:
+    #         self.vel.y = -PLAYER_JUMP
     def update(self):
-        self.acc = self.vel * PLAYER_FRICTION
+        self.acc = vec(0, PLAYER_GRAV)
+
+        self.acc.x = self.vel.x * PLAYER_FRICTION
         self.input()
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
+        self.rect.midbottom = self.pos
         self.rect.center = self.pos
         if self.rect.x > WIDTH:
             print("I'm off the right screen...")
@@ -51,17 +68,25 @@ class Mob(Sprite):
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(1,0)
+        self.vel = vec(randint(1,5),randint(1,5))
         self.acc = vec(0,0)
         self.cofric = 0.1
         self.canjump = False
     def behavior(self):
-        if self.rect.x > WIDTH or self.rect.x < 0:
-            self.vel.x *= -1
-        if self.rect.y > HEIGHT or self.rect.y < 0:
-            self.vel.y *= -1
-        
+        if self.rect.x > WIDTH or self.rect.x < 0 or self.rect.y > HEIGHT or self.rect.y < 0:
+            self.vel *= -1
+
     def update(self):
         self.behavior()
         self.pos += self.vel
         self.rect.center = self.pos
+        
+class Platform(Sprite):
+    def __init__(self, x, y, w, h):
+        Sprite.__init__(self)
+        self.image = pg.Surface((50,50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x 
+        self.y = y 
+        
